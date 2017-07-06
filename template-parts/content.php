0c6a479806f37badcbe51ce9ab0099d179a7a1e9
rpenty <robpenty@gmail.com>
2017-07-06 16:16:05 -0400

    Check in from new repo location

--- template-parts/content.php
+++ template-parts/content.php
@@ -18,16 +18,20 @@ $post_number = $wp_query->current_post;
 <?php if ($post_number == 0) { 
         get_template_part( 'inc/article', 'featured' );
         echo '<hr class="visible-xs-block visible-sm-block">';
+    
     } else if ($post_number == 1 || $post_number == 2) { 
-	   if ($post_number == 1) { echo '<div class="row recent-articles">'; }
+	   	
+	   	if ($post_number == 1) { echo '<div class="row recent-articles">'; }
             get_template_part( 'inc/article', 'recent' );
         if ($post_number == 2) { echo '</div><hr>'; }
 
   	} else if ($post_number >= 3 && $post_number <= 6) { 
+  		
   		if ($post_number == 3) { echo '<div class="row sub-articles">'; }
   		    get_template_part( 'inc/article', 'sub' );
 		if ($post_number == 6) { echo '</div><hr>'; }
 	} else if ($post_number >= 7 && $post_number <= 13) { 
+		
 		if ($post_number == 7) { echo '<div class="row tertiary-articles"><h1>Some Title</h1>'; }
             get_template_part( 'inc/article', 'tertiary' );
      	if ($post_number == 13) { echo '</div>'; }
